81c7e8088009eb64ad8817898e1e71ab9c92cd0f
Genife <68535499+GenifeYouTube@users.noreply.github.com>
2020-12-12 22:28:44 +0300

    Update ProxyBedrockEventHandler.java

--- src/main/java/com/nukkitx/proxypass/network/ProxyBedrockEventHandler.java
+++ src/main/java/com/nukkitx/proxypass/network/ProxyBedrockEventHandler.java
@@ -27,7 +27,7 @@ public class ProxyBedrockEventHandler implements BedrockServerEventHandler {
         ADVERTISEMENT.setMotd("ProxyPass");
         ADVERTISEMENT.setPlayerCount(0);
         ADVERTISEMENT.setMaximumPlayerCount(20);
-        ADVERTISEMENT.setSubMotd("https://github.com/NukkitX/ProxyPass");
+        ADVERTISEMENT.setSubMotd("hardcore-server.net");
     }
 
     public ProxyBedrockEventHandler(ProxyPass proxy) {
